fa4d874f3a0d1adf1eb979d06a0f7ba382dc5434
Dipanjan Kailthya <dipanjan.kailthya@gmail.com>
2021-01-21 04:45:55 +0100

    fix request body processing

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 import json
 
 from chalice import Chalice
-
+from chalicelib.forecaster import average_forecast
 app = Chalice(app_name='avg_forecaster')
 
 
 @app.route('/forecast', methods=['POST'])
 def forecast():
     forecast_data = app.current_request.json_body
-    data_dict = json.loads(forecast_data)
-    result = make_forecast(data_dict)
+    result = average_forecast(forecast_data)
     return json.dumps(result)
