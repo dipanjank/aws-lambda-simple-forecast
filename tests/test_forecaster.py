d9265f6a9a51016dae5cf56ad894cf35349e4b1d
Dipanjan Kailthya <dipanjan.kailthya@gmail.com>
2021-01-21 05:47:12 +0100

    Fix int datatype

--- tests/test_forecaster.py
+++ tests/test_forecaster.py
@@ -1,14 +1,15 @@
+import numpy as np
+
 from chalicelib.forecaster import average_forecast
+
+RAND = np.random.RandomState(100)
 
 
 def test_average_forecast():
-    train_data = [
-        {
-            'date': f'2020-01-{str(n).zfill(2)}',
-            'data': 10.0
-        }
-        for n in range(1, 31+1)
-    ]
+    values = RAND.randint(low=50, high=200, size=31)
+    dates = [f'2020-01-{str(n).zfill(2)}' for n in range(1, 31+1)]
+
+    train_data = [{'date': d, 'data': v} for d, v in zip(dates, values)]
 
     data_dict = {
         'train_data': train_data,
@@ -18,7 +19,7 @@ def test_average_forecast():
     actual = average_forecast(data_dict)
     expected = [{
         'date': f'2020-02-{str(n).zfill(2)}',
-        'data': 10.0
+        'data': 134.48387096774192
     } for n in range(1, 29+1)]
 
     assert expected == actual
